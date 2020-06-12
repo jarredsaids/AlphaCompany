444855a6335ff0093f054b2dc4183f21bcc50bec
David Turner <jarredsaids@gmail.com>
2020-06-12 14:46:44 -0400

    begin work on home route

---
+++ resources/views/home/main.blade.php
@@ -0,0 +1,31 @@
+@extends('app')
+
+@section('content')
+
+
+    <h1>Aco Business</h1>
+    <hr>
+
+    <div id="choose">
+        <div class="selection"><a href="">
+                <div class="selection-title">DS Candidates</div>
+            </a>
+        </div>
+        <div class="selection"><a href="">
+                <div class="selection-title">Metrics</div>
+            </a>
+        </div>
+        <div class="selection"><a href="">
+                <div class="selection-title">NCOERs</div>
+            </a>
+        </div>
+        <div class="selection"><a href="">
+                <div class="selection-title">Classes</div>
+            </a>
+        </div>
+    </div>
+    </div>
+
+
+
+@endsection

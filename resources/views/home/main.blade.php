1449f5071bf45eb8aa720fc07728e8aeeb6f0aa4
David Turner <jarredsaids@gmail.com>
2020-06-12 15:20:20 -0400

    fix images

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -7,19 +7,27 @@
     <hr>
 
     <div id="choose">
-        <div class="selection"><a href="">
+        <div class="selection">
+            <a href="">
+                <img src="{{ asset('images/badge.jpg') }}" alt="">
                 <div class="selection-title">DS Candidates</div>
             </a>
         </div>
-        <div class="selection"><a href="">
+        <div class="selection">
+            <a href="">
+                <img src="{{ asset('images/dueouts.png') }}" alt="">
                 <div class="selection-title">Metrics</div>
             </a>
         </div>
-        <div class="selection"><a href="">
+        <div class="selection">
+            <a href="">
+                <img src="{{ asset('images/ncoer_meme.jpg') }}" alt="">
                 <div class="selection-title">NCOERs</div>
             </a>
         </div>
-        <div class="selection"><a href="">
+        <div class="selection">
+            <a href="">
+                <img src="{{ asset('images/classes.jfif') }}" alt="">
                 <div class="selection-title">Classes</div>
             </a>
         </div>
